a932d4692f4b8d5d93ebdc65650b6ca7f5e3b429
Shriram P <27760040+shriram-pasrija@users.noreply.github.com>
2023-10-29 11:11:39 +0100

    initital commit

---
+++ src/main/java/com/example/mlpnotificationservice/MlpNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mlpnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlpNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlpNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mlpnotificationservice/MlpNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mlpnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlpNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
